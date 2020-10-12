4804631c8b35b78351d81adaa9d60be04060c174
Nitish <70720230+nitishmohan8@users.noreply.github.com>
2020-10-13 01:30:36 +0530

    Add files via upload

---
+++ JS-FW-Task-2/CountriesFlagURL.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest() 
+
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true)
+request.send();
+
+request.onload = function() {
+
+    var jsonData = JSON.parse(this.response) 
+    var flag = [] 
+   for(var i=0;i<jsonData.length;i++){
+   	console.log(jsonData[i].flag)
+   }
+    
+    
+    };
+
+
+
